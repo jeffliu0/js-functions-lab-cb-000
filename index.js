fbaea5a74dfe0c3775c9aed5ab254cafc193a4a2
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-16 23:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidaysTo(name, holiday){
   return `Happy ${holiday}, ${name}!`
 }
 
